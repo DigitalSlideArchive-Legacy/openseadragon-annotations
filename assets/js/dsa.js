a6c52bb18b067976f7439e0c6d81acfdcd6fb200
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-01-29 14:18:01 -0500

    Removed some redundant code

--- assets/js/dsa.js
+++ assets/js/dsa.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
 	var viewer = DSAViewer.getViewer();
-		var annotationState = window.annotationState = new AnnotationState();
-		annotationState.setSeadragonViewer(viewer);
-		annotation_setup_code(annotationState);
+	var annotationState = window.annotationState = new AnnotationState();
+	annotationState.setSeadragonViewer(viewer);
+	annotation_setup_code(annotationState);
 	
-		$(annotationState).on("annotationAdded", function(e) {
-
-			//Once an annotation is added update the markup controller variable
-    		var scope = angular.element(document.getElementById("annotation_list_ctrl")).scope();
+	$(annotationState).on("annotationAdded", function(e) {
+		//Once an annotation is added update the markup controller variable
+		var scope = angular.element(document.getElementById("annotation_list_ctrl")).scope();
 			
-			//Add new markup
-			//Use $apply, which will trigger, $digest and $watch to update all scope variables.
-			scope.$apply(function () {
-				scope.add(annotationState.annotations);
-			});
+		//Add new markup
+		//Use $apply, which will trigger, $digest and $watch to update all scope variables.
+		scope.$apply(function () {
+			scope.add(annotationState.annotations);
+		});
 	});
 
 	$("#viewport_properties_modal").draggable({
-      handle: ".modal-header"
+		handle: ".modal-header"
 	});
 
 	$("#annotations_modal").draggable({
-      handle: ".modal-header"
+		handle: ".modal-header"
 	});
 
 	$("#draw_tools_modal").draggable({
-      handle: ".modal-header"
+		handle: ".modal-header"
 	});
 
 	$("#layers_modal").draggable({
-      handle: ".modal-header"
+		handle: ".modal-header"
 	});
 });
