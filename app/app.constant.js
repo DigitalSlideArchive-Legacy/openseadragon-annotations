a9847d4b0279cdcbe1d6fd99b4bfbb22e1c19cf9
Mohammed Khalilia <mohammed.khalilia@gmail.com>
2016-02-10 13:11:30 -0500

    Use cloudeval VM

--- app/app.constant.js
+++ app/app.constant.js
@@ -1,7 +1,7 @@
 //Define constant values that can be used throughout AngularJS
 app.constant('api', 
 	{
-		url: 'http://localhost',
+		url: 'http://cloudeval.neuro.emory.edu',
 		port: 5003
 	});
 
